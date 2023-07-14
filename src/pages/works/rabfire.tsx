fd72a8d9b62fe93e91b3d22091c7bcf73fc09e18
woicip <woicipta@gmail.com>
2023-07-14 10:49:22 +0700

    update: rabfire info project

--- src/pages/works/rabfire.tsx
+++ src/pages/works/rabfire.tsx
@@ -61,7 +61,7 @@ export default function Rabfire(props: any){
 
                         <div>
                             <p className="font-light text-[15px]">Timeline</p>
-                            <p className="mt-[5px] tabletM:text-[15px]">Feb - Jun 2023</p>
+                            <p className="mt-[5px] tabletM:text-[15px]">Feb ~ Jun 2023</p>
                         </div>
 
                         <motion.div className="text-left py-[10px] px-[13px] tabletL:px-0 hover:bg-[#f3f3f3] tabletL:hover:bg-transparent rounded-2xl transition-all overflow-hidden">
