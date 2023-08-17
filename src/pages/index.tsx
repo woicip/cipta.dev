2ed4b7120e9f1fb15a8cfe00b70ce2bf9f1c41f0
woicip <woicipta@gmail.com>
2023-08-17 16:20:05 +0700

    update: remove "motion" import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { m, motion, AnimatePresence } from "framer-motion";
+import { m, AnimatePresence } from "framer-motion";
 
 // components
 import Project from "@/components/Projects/Project";
